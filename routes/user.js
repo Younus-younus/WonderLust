3f5e95d6ba43fcfb881c3a6ba0194baaa822da18
Younus <you@example.com>
2024-09-08 12:44:46 +0530

    initial commit

--- routes/user.js
+++ routes/user.js
@@ -1,50 +1,55 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
-const userSchema = require("../model/user.js");
+const router = express.Router({ mergeParams: true });
+const User = require("../model/user.js"); // Correct model reference
 const WrapAsync = require("../utils/WrapAsync");
 const passport = require("passport");
+const { saveUrl } = require("../views/middleware.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs")
+// Signup Routes
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
 });
 
-router.post("/signup",WrapAsync(async(req,res)=>{
-    try  {
-        const {username,email,password} = req.body;
-    const newUser = new userSchema({email,username});
-    const registeredUser= await userSchema.register(newUser,password);
-    req.login(registeredUser,(err)=> {
-        if(err) {
-           return next(err);
-        }
-        req.flash("success","Welcome to WonderLust!");
-        res.redirect("/listings");
-        })
-    }
-    catch(e) {
-        req.flash("error",e.message);
+router.post("/signup", WrapAsync(async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+        const newUser = new User({ email, username }); // Ensure correct model usage
+        const registeredUser = await User.register(newUser, password);
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to WonderLust!");
+            res.redirect("/listings");
+        });
+    } catch (e) {
+        req.flash("error", e.message);
         res.redirect("/signup");
     }
 }));
 
-router.get("/login",(req,res)=>{
+// Login Routes
+router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
-router.post("/login",passport.authenticate("local",
-    {failureRedirect: "/login", failureFlash: true}),
-(req,res)=>{
-    req.flash("success","Welcome back to WonderLust");
-    res.redirect("/listings");
+router.post("/login", saveUrl, passport.authenticate("local", 
+    { failureRedirect: "/login", failureFlash: true }),
+(req, res) => {
+    req.flash("success", "Welcome back to WonderLust");
+    const redirectUrl = res.locals.redirectUrl || '/listings'; // Provide a default redirect
+    res.redirect(redirectUrl);
 });
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=> {
-        if(err) {
-           return next();
+
+// Logout Route
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
         }
-        req.flash("succeess","You are Logged Out!");
+        req.flash("success", "You are Logged Out!");
         res.redirect("/listings");
-})
+    });
 });
 
 module.exports = router;
--- views/middleware.js
+++ views/middleware.js
@@ -1,7 +1,15 @@
-module.exports.isLoggedIn=(req,res,next)=> {
-    if(!req.isAuthenticated()) {
-        req.flash("error","You must be logged in!");
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "You must be logged in!");
         return res.redirect("/login");
     }
     next();
 }
+
+module.exports.saveUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
